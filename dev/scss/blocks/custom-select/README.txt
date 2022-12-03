786f008c6b8e5d208f2328463591fed9b014b51e
Predare <predare12000@gmail.com>
2022-12-03 20:42:15 +0300

    Init

---
+++ dev/scss/blocks/custom-select/README.txt
@@ -0,0 +1,2 @@
+Docs: https://itchief.ru/javascript/custom-select
+Git: https://github.com/itchief/ui-components/tree/master/custom-select
---
+++ dev/scss/blocks/custom-select/signup-select-inits.js
@@ -0,0 +1,5 @@
+﻿document.addEventListener('DOMContentLoaded', () => {
+    new ItcCustomSelect('#select-race');
+    new ItcCustomSelect('#select-class');
+    new ItcCustomSelect('#select-gender');
+})
